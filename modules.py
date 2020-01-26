944bd813ba403acf54f90e3092b7121dfc8a0998
ArgEric8 <ericnilden@gmail.com>
2020-01-26 08:35:17 +0100

    fin

--- modules.py
+++ modules.py
@@ -1,39 +1,44 @@
 import random
 
 def divideNumbers(divideFirst, divideSecond):
-    numberList = (range(1, 1001))
+    numberList = range(1, 1001)
     array = []
+    total = 0
     for x in numberList:
         if x % divideFirst == 0 and x % divideSecond == 0:
             array.append(x)
-            total = 0
-            for x in array:
-                total += x
+            total += x
+    
+            
     print(array)
     print("Medelvärdet är", total / len(array))
 
+
 def randomTal(firstNumber = 1, secondNumber = 100):
+    number = random.randint(firstNumber, secondNumber)
     maxAttempts = secondNumber // 3
-    print(f"\n\nGissa rätt tal mellan {firstNumber}-{secondNumber}, du har max {maxAttempts} försök.")
-    print("Avbryt med 0")
     guess = 0
     attempt = 0
-    number = random.randint(firstNumber, secondNumber)
+    print(f"\n\nGissa rätt tal mellan {firstNumber}-{secondNumber}, du har max {maxAttempts} försök.")
+    print("Avbryt med 0")
+    #fusk > print(number)
+    
     while guess != number:
         guess = userInputInt()
-        if guess == 0:
+        attempt += 1
+        if guess == 0: # avsluta
             break
-        if guess < number:
+        elif guess < number:
             print("Talet är större")
         elif guess > number:
             print("Talet är mindre")
         elif guess == number:
             print("\nWow! Du har vunnit!")
             break
-        attempt += 1
         if attempt == maxAttempts:
             print("\nDu har förlorat...")
             break
+
 
 def userInputInt():
     while True:
